e371942af5b17129dfa7f2972b5fdd9810df2450
Bernard Gray <bernard.gray@gmail.com>
2022-08-01 06:57:44 +1000

    tidy up front, add links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,8 +4,7 @@ import Styles from './styles.module.scss';
 function Footer() {
 	return (
 		<footer className={Styles.footer}>
-			&copy; {new Date().getFullYear()} Jeanine White
-			<small className={Styles.byline}>🚀 Built by Astro</small>
+			{new Date().getFullYear()}
 		</footer>
 	);
 }
